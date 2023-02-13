7c256d4bcee018bf6362088f206830d6c8a09963
Dimitri Lhopital <dimitrilho.gmail.com>
2023-02-13 14:13:31 +0400

    feat: ✨Add Event for search feature

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,11 @@
-const Search = () => {
-	return <input type="text" className="input-search" />
+import React from 'react'
+
+type SearchProps = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Search = ({onChange}: SearchProps) => {
+  return <input type="text" className="input-search" onChange={onChange} />
 }
 
 export {Search}
